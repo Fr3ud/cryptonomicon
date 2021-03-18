bcb1da883b3c917460e30d3761f2d3218f051ea9
Fr3ud <oo.pavelko@gmail.com>
2021-03-18 17:06:17 +0200

    feat: fetch data from api

--- src/App.vue
+++ src/App.vue
@@ -49,6 +49,8 @@
           <div
             v-for="t in tickers"
             :key="t.name"
+            @click="select = t"
+            :class="{ 'border-4': select === t }"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
@@ -61,7 +63,7 @@
             </div>
             <div class="w-full border-t border-gray-200"></div>
             <button
-              @click="handleDelete(t)"
+              @click.stop="handleDelete(t)"
               class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
               <svg
@@ -84,9 +86,9 @@
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
 
-      <section class="relative">
+      <section v-if="select" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          VUE - USD
+          {{ select.name }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div class="bg-purple-800 border w-10 h-24"></div>
@@ -94,7 +96,11 @@
           <div class="bg-purple-800 border w-10 h-48"></div>
           <div class="bg-purple-800 border w-10 h-16"></div>
         </div>
-        <button type="button" class="absolute top-0 right-0">
+        <button
+          @click="select = null"
+          type="button"
+          class="absolute top-0 right-0"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -123,28 +129,36 @@
 </template>
 
 <script>
+import API_KEY from "../config";
+
 export default {
   name: "App",
 
   data() {
     return {
       ticker: "",
-      tickers: [
-        { name: "Demo-1", price: "42" },
-        { name: "Demo-2", price: "42" },
-        { name: "Demo-3", price: "42" },
-      ],
+      tickers: [],
+      select: null,
     };
   },
 
   methods: {
     add() {
       const newTicker = {
-        name: this.ticker,
-        price: "42",
+        name: this.ticker.toUpperCase(),
+        price: "",
       };
 
       this.tickers.push(newTicker);
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${newTicker.name}&tsyms=USD&api_key=${API_KEY}`
+        );
+        const data = await f.json();
+        const { USD } = data;
+        this.tickers.find((ticker) => ticker.name === newTicker.name).price =
+          USD > 1 ? USD.toFixed(2) : USD.toPrecision(2);
+      }, 3000);
       this.ticker = "";
     },
 
